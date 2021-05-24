68c935bacd8dc1283cf81f0711ff9b7c1a3a59ee
ptakchun <ptakchun@connect.hku.hk>
2021-05-24 23:17:33 +0800

    Updated divPre

--- divPre.py
+++ divPre.py
@@ -26,8 +26,6 @@ crsp_df = pd.read_csv("crsp.zip", compression='zip',header=0,
 #Data Preprocessing
 crsp_df = crsp_df[(crsp_df.SHRCD.isin(('10','11')))]
 crsp_df = crsp_df[(crsp_df.date <= '2011-12-31')]
-cusips_DISTCD_12 = crsp_df[crsp_df.DISTCD.apply(lambda x: x[:2]=='12' if isinstance(x, str) else False)].CUSIP.unique()
-crsp_df = crsp_df[crsp_df.CUSIP.isin(cusips_DISTCD_12)]
 
 # crsp_df =  crsp_df[( crsp_df['RET'].apply(lambda x: str(x)[-1].isdigit()) )]
 # crsp_df['RET'] = crsp_df['RET'].astype('float64')
@@ -44,6 +42,8 @@ crsp_df.head()
 
 #In[]
 crsp_div_df = crsp_df[['date','CUSIP','DCLRDT','RCRDDT','DISTCD','DIVAMT','PRC','VOL','RET','SHROUT','SPREAD','year','month','PRC_t-1','RETX']].copy()
+# cusips_DISTCD_12 = crsp_div_df[crsp_div_df.DISTCD.apply(lambda x: x[:2]=='12' if isinstance(x, str) else False)].CUSIP.unique()
+# crsp_div_df = crsp_div_df[crsp_div_df.CUSIP.isin(cusips_DISTCD_12)]
 crsp_div_df.sort_values(by=['CUSIP','date'], ascending=True, inplace=True)
 crsp_div_df = crsp_div_df.groupby(by=['CUSIP','date']).agg({
      'DCLRDT': 'last',
@@ -56,38 +56,81 @@ crsp_div_df = crsp_div_df.groupby(by=['CUSIP','date']).agg({
      'PRC': 'last',
      'PRC_t-1': 'last',
      'SPREAD': 'last',
-     'SHROUT': 'last'})
+     'SHROUT': 'last',
+     'year':'last'})
 crsp_div_df['MCAP'] = crsp_div_df['PRC'] * crsp_div_df['SHROUT']
+crsp_div_df['TURNOVER'] = crsp_div_df['VOL'] / crsp_div_df['SHROUT']
+# crsp_div_df.to_pickle('./crsp_div_df_afterGroupBy.pkl.zip', compression='zip')
 crsp_div_df
-
-#In[]
-crsp_div_df.to_pickle('./crsp_div_df_afterGroupBy.pkl.zip', compression='zip')
-#In[]
+#In[] Find months with dividend past year
 crsp_div_df.sort_values(by=['CUSIP','date'], ascending=True, inplace=True)
 crsp_div_df['freq'] = None
-crsp_div_df['freq'] = crsp_div_df[crsp_div_df.RCRDDT.isna()==False]['DISTCD'].apply(lambda x: x[2:3] if x is not None else None)
+# crsp_div_df['freq'] = crsp_div_df[crsp_div_df.RCRDDT.isna()==False]['DISTCD'].apply(lambda x: x[2:3] if x is not None else None)
+crsp_div_df['freq'] = crsp_div_df[crsp_div_df.DIVAMT != 0.0]['DISTCD'].apply(lambda x: x[2] if x is not None and x[:2]=='12' else None)
 crsp_div_df['freq'] = crsp_div_df.groupby(by=['CUSIP']).fillna(method='ffill', limit=11)['freq']
 
 #In[]
-crsp_div_df.loc[:,['RCRDDT','DIVAMT','DISTCD','freq']][:400]
+# crsp_div_df.loc[:,['RCRDDT','DIVAMT','DISTCD','freq']][:400]
 # crsp_div_df['DISTCD'].apply(lambda x: x[2:3] if x is not None else None)[:400]
 
 #In[]
-crsp_div_df['MCAP'] = crsp_div_df['PRC'] * crsp_div_df['SHROUT']
-crsp_div_df['TURNOVER'] = crsp_div_df['VOL'] / crsp_div_df['SHROUT']
-
 crsp_div_df['div_yield'] = crsp_div_df['DIVAMT'].apply(lambda x: 0.0 if x.isna() else x)
 
+#In[] load compustat bkvlps and merge to crsp_div_df
+compustat_df = pd.read_csv("compustat.zip", compression='zip', header=0,
+     parse_dates = ['datadate'],dtype={
+          'fyear':np.float64,
+          'cusip': str,
+          'bkvlps': np.float64
+     })
+compustat_df = compustat_df[(compustat_df.indfmt != 'FS') & (compustat_df.curcd=='USD')]
+compustat_df.dropna(subset=['fyear','cusip'], inplace=True)
+compustat_df['CUSIP'] = compustat_df['cusip'].apply(lambda x: None if pd.isna(x) else x[:-1]) 
+compustat_df.fyear = compustat_df.fyear.astype(np.int64)
+compustat_df['year'] = compustat_df.fyear.apply(lambda x: x+1)
+cc_df = pd.merge(crsp_div_df.reset_index(), compustat_df[['CUSIP','year','bkvlps']], on=['CUSIP','year'], how='left')
+cc_df.set_index(['CUSIP', 'date'], inplace=True)
+cc_df['BM'] =  cc_df['bkvlps']/cc_df['PRC'] 
 
-#In[] panel A & B
-fil_A = (crsp_div_df.freq.isna() == False) & (crsp_div_df["PRC_t-1"].isna()==False) & (crsp_div_df["PRC_t-1"] >= 5) & (crsp_div_df.freq < '6')
-fil_B = (crsp_div_df.freq.isna() == True) |  (crsp_div_df.freq >= '6')
+#In[]
+cc_df
 
-for fil in [fil_A,fil_B]:
-     print(crsp_div_df[fil][['MCAP','TURNOVER','SPREAD']].describe())
-     print('Number of Firm Months', len(crsp_div_df[fil]))
-     print('Number of Firms', len(pd.Series(crsp_div_df.index.get_level_values('CUSIP')).unique() ))
+#In[] Table 1 panel A & B & C
+cusips_DISTCD_12 = pd.Series(cc_df[cc_df.DISTCD.apply(lambda x: x[:2]=='12' if isinstance(x, str) else False)].index.get_level_values('CUSIP')).unique()
 
-#In[] Panel C:
-s = crsp_div_df[fil_A].freq
-s.groupby(s).count()/1494129 * 100
+
+
+
+panel_A = cc_df.loc[pd.IndexSlice[cusips_DISTCD_12,:]]
+fil_A = (panel_A["PRC_t-1"] >= 5) & (panel_A.freq < '6') & (panel_A.freq != '2')
+print("Panel A - Firms with a Dividend in the Past Year")
+print(panel_A[fil_A][['MCAP','BM','TURNOVER','SPREAD']].describe())
+print('Number of Firm Months', len(panel_A[fil_A]))
+print('Number of Firms', len(pd.Series(panel_A.index.get_level_values('CUSIP')).unique() ))
+
+fil_B = ((cc_df.freq.isna() == True) |  (cc_df.freq >= '6')) & (cc_df["PRC_t-1"] >= 5)
+panel_B = cc_df[fil_B]
+print("Panel B - Firms with No Dividend in the Past Year")
+print(panel_B[['MCAP','BM','TURNOVER','SPREAD']].describe())
+print('Number of Firm Months', len(panel_B))
+print('Number of Firms', len(pd.Series(panel_B.index.get_level_values('CUSIP')).unique() ))
+
+#Panel C:
+
+fil_C = (cc_df["PRC_t-1"] >= 5) & (cc_df.freq < '6')
+s = cc_df[fil_C].freq
+freq_dist = s.groupby(s).count()/ s.groupby(s).count().sum() * 100
+panel_C = {
+     'Monthly': freq_dist[2],
+     'Quarterly': freq_dist[3],
+     'Semi-Annual': freq_dist[4],
+     'Annual': freq_dist[5],
+     'Unknown Frequency': freq_dist[0] + freq_dist[1]
+}
+print("\nPanel C: Pct of Dividend Observations (%)")
+print(pd.Series(panel_C))
+
+#In[]
+cusips_DISTCD_12 = pd.Series(cc_df[cc_df.DISTCD.apply(lambda x: x[:2]=='12' if isinstance(x, str) else False)].index.get_level_values('CUSIP')).unique()
+cc_df.loc[pd.IndexSlice[cusips_DISTCD_12,:]]
+len(cusips_DISTCD_12)
